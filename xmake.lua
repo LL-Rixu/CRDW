e1dc07a389e81e1796123d722a71aec86a448468
Rixu <raxuti333@gmail.com>
2026-07-29 15:23:11 +0300

    Build options for AE, SE and VR

--- xmake.lua
+++ xmake.lua
@@ -1,16 +1,22 @@
 set_xmakever("3.0.7")
 
 local PROJECT_NAME = "CRDW"
-local PROJECT_VERSION = "1.1.0"
+local PROJECT_VERSION = "1.1.1b"
 
 set_project(PROJECT_NAME)
 set_version(PROJECT_VERSION)
+
+option("version")
+    set_default("AE")
+    set_values("AE", "SE", "VR")
+    set_showmenu(true)
+    set_description("Controls build version")
+option_end()
 
 set_languages("c++23")
 set_arch("x64")
 set_encodings("utf-8")
 
-add_defines("SKYRIM_SUPPORT_AE")
 includes("extern/CommonLibSSE")
 
 add_rules("mode.release", "mode.releasedbg", "mode.debug")
@@ -21,6 +27,18 @@ set_runtimes(is_mode("debug") and "MTd" or "MT")
 
 target(PROJECT_NAME)
     set_kind("shared")
+
+    if is_config("version", "AE") then
+        add_defines("SKYRIM_SUPPORT_AE")
+        PROJECT_VERSION = "AE " .. PROJECT_VERSION
+        PROJECT_NAME = PROJECT_NAME .. "_AE"
+    elseif is_config("version", "SE") then
+        PROJECT_VERSION = "SE " .. PROJECT_VERSION
+        PROJECT_NAME = PROJECT_NAME .. "_SE"
+    elseif is_config("version", "VR") then
+        PROJECT_VERSION = "VR " .. PROJECT_VERSION
+        PROJECT_NAME = PROJECT_NAME .. "_VR"
+    end
 
     add_deps("commonlibsse")
 
