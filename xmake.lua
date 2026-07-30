dc7b0739dbfbf1832da379f6451b32a69f3635f2
Rixu <raxuti333@gmail.com>
2026-07-30 16:10:35 +0300

    bumped version to 1.1.3 added automatic pdb packing

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,8 @@
 set_xmakever("3.0.7")
 
 local PROJECT_NAME = "CRDW"
-local PROJECT_VERSION = "1.1.2"
-local PROJECT_STAGE = "Beta"
+local PROJECT_VERSION = "1.1.3"
+local PROJECT_STAGE = ""
 
 set_project(PROJECT_NAME)
 set_version(PROJECT_VERSION)
@@ -38,13 +38,13 @@ target(PROJECT_NAME)
         build = "VR "
     end
 
-    add_defines("PROJECT_NAME=\"" .. PROJECT_NAME .. " " .. PROJECT_STAGE .. "\"")
+    add_defines("PROJECT_NAME=\"" .. PROJECT_NAME .. PROJECT_STAGE .. "\"")
     add_defines("PROJECT_VERSION=\"" .. build .. PROJECT_VERSION .. "\"")
 
     add_deps("commonlibsse")
 
     add_rules("commonlibsse.plugin", {
-        name = PROJECT_NAME .. " " .. PROJECT_STAGE,
+        name = PROJECT_NAME .. PROJECT_STAGE,
         author = "Rixu",
         version = PROJECT_VERSION,
         description = "Caches the Recrusive Directory Walk"
@@ -61,25 +61,33 @@ target(PROJECT_NAME)
 after_build(function (target)
         local build_dir = target:targetdir()
         local dll_path = target:targetfile()
+        local pdb_path = target:symbolfile()
         local ini_path = path.join(os.projectdir(), "CRDW.ini")
         
         local staging_dir = path.join(build_dir, "staging")
         local plugin_dir = path.join(staging_dir, "Data/SKSE/Plugins")
 
-        local build = "AE "
+        local build = " AE"
         if is_config("version", "SE") then
-            build = "SE "
+            build = " SE"
         elseif is_config("version", "VR") then
-            build = "VR "
+            build = " VR"
         end
 
         os.tryrm(staging_dir)
         os.mkdir(plugin_dir)
 
         os.cp(dll_path, plugin_dir)
-        os.cp(ini_path, plugin_dir)
 
-        local archive_file = path.absolute(path.join(build_dir, build .. target:name() .. ".7z"))
+        if os.isfile(pdb_path) then
+            os.cp(pdb_path, plugin_dir)
+        end
+
+        if os.os.isfile(ini_path) then
+            os.cp(ini_path, plugin_dir)
+        end
+
+        local archive_file = path.absolute(path.join(build_dir, target:name() .. build .. ".7z"))
         
         os.tryrm(archive_file)
     
