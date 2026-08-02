bb69a7de67abf5252e4d9ada49a7a438a0708e6d
Rixu <raxuti333@gmail.com>
2026-08-03 01:38:17 +0300

    Added VR Offsets. Changed Hook to use GetAddress

--- include/ID.h
+++ include/ID.h
@@ -2,6 +2,8 @@
 
 #ifdef SKYRIM_SUPPORT_AE
 
+#define USE_RELID true
+
 #define WalkerID 70050
 #define GetSizeID 70164
 
@@ -13,7 +15,24 @@
 #define InsertWithCacheID 69805
 #define UpdateCacheID 69939
 
+#elif SKYRIM_SUPPORTS_VR
+
+#define USE_RELID false
+
+#define WalkerID 0xC92580
+#define GetSizeID 0xC96E00
+
+#define EventSourcesID 0x3169BC0
+#define CacheID 0x3169BC8
+
+#define StripPrefixID 0xC8ABB0
+#define InsertNoCacheID 0xC83390
+#define InsertWithCacheID 0xC88E50
+#define UpdateCacheID 0xC8D660
+
 #else
+
+#define USE_RELID true
 
 #define WalkerID 68707
 #define GetSizeID 68812
--- include/hook.h
+++ include/hook.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <ID.h>
 #include <RE/Skyrim.h>
 #include <log.h>
 
@@ -9,10 +10,19 @@ class Hook
     bool loaded = false;
     std::vector<uint8_t> bytes;
 
+    constexpr uintptr_t GetAddress() const
+    {
+    #if USE_RELID == true
+        return REL::ID(id).address();
+    #else
+        return REL::Offset(id);
+    #endif
+    } 
+
 public:
     void Install(std::vector<uint8_t> payload)
     {
-        const uintptr_t address = REL::ID(id).address();
+        const uintptr_t address = GetAddress();
 
         bytes.reserve(payload.size());
         std::copy_n(reinterpret_cast<uint8_t*>(address), payload.size(), std::back_inserter(bytes));
@@ -25,7 +35,8 @@ public:
     {
         if(!loaded) { return; }
 
-        const uintptr_t address = REL::ID(id).address();
+        const uintptr_t address = GetAddress();
+
         REL::safe_write(address, bytes);
 
         bytes.clear();
@@ -34,20 +45,25 @@ public:
 
     ~Hook() { Remove(); }
 
-    std::span<uint8_t> Binary() const { return { reinterpret_cast<uint8_t*>(REL::ID(id).address()), bytes.size() }; }
+    std::span<uint8_t> Binary() const 
+    {
+        return { reinterpret_cast<uint8_t*>(GetAddress()), bytes.size() }; 
+    }
 
     template<auto function>
     auto Native()
     {
         if(loaded) { Remove(); }
-        return reinterpret_cast<decltype(function)>(REL::ID(id).address()); 
+
+        return reinterpret_cast<decltype(function)>(GetAddress()); 
     }
 
     template<typename T>
     auto Native()
     {
         if(loaded) { Remove(); }
-        return reinterpret_cast<T>(REL::ID(id).address()); 
+
+        return reinterpret_cast<T>(GetAddress()); 
     }
 };
 
